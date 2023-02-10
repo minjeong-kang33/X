9e9b2b5506c580013505e9e0598127a81a1dd22c
seungmin <sm.lee2026210@gmail.com>
2023-02-10 20:53:16 +0900

    review DAO, DTO 수정 (미완성) review 게시판번호 컬럼명 수정

--- team1_ojjang/src/main/java/review/reviewDAO.java
+++ team1_ojjang/src/main/java/review/reviewDAO.java
@@ -3,6 +3,7 @@ package review;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -25,7 +26,6 @@ public class reviewDAO {
 		PreparedStatement pstmt = null;
 		ResultSet rs=null;
 		
-		
 		try {
 			con=getConnection();
 			
@@ -42,8 +42,8 @@ public class reviewDAO {
 			
 			//게시판 DB입력
 			
-			sql = "insert into board(RE_id,S_id,RE_writer,RE_title,RE_text,RE_createtime,RE_view"
-					+ "RE_img1,RE_delivery,RE_manner,RE_ProductStatus,RE_fast,RE_time) value (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			sql = "insert into board(RE_num,S_id,RE_writer,RE_title,RE_text,RE_createtime,RE_view"
+					+ "RE_img1,RE_delivery,RE_manner,RE_ProductStatus,RE_fast,RE_time) value (?,?,?,?,?,?,?,?,?,?,?,?,?)";//13개
 			pstmt = con.prepareStatement(sql);
 			
 			pstmt.setInt(1, rE_id);
@@ -55,17 +55,71 @@ public class reviewDAO {
 			pstmt.setInt(7, dto.getRE_view());
 			pstmt.setString(8, dto.getRE_img1());
 			pstmt.setString(9, dto.getRE_delivery());
-			pstmt.setString(10, dto.RE_img1());
-			pstmt.setString(11, dto.RE_img1());
+			pstmt.setString(10, dto.getRE_manner());
+			pstmt.setString(11, dto.getRE_ProductStatus());
+			pstmt.setString(12, dto.getRE_fast());
+			pstmt.setString(13, dto.getRE_time());
 			
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt!=null) try {pstmt.close();} catch (Exception e2) {e2.printStackTrace();}
+			if(con!=null) try {con.close();} catch (Exception e2) {e2.printStackTrace();}
+			if(rs!=null) try {rs.close();} catch (Exception e2) {e2.printStackTrace();}
+		}
+	}//reviewWriteBoard 끝 (리뷰글쓰기)
 	
+	
+	public ArrayList<reviewDTO> getReviewList(){
+		ArrayList<reviewDTO> reviewList = new ArrayList<reviewDTO>();
+		
+		reviewDTO dto = null;
+		PreparedStatement pstmt = null;
+		Connection con = null;
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
 			
+			String sql="select * from board where id=?";
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt!=null) try {pstmt.close();} catch (Exception e2) {}
+			if(con!=null) try {con.close();} catch (Exception e2) {}
+			if(rs!=null) try {rs.close();} catch (Exception e2) {}
+		}
+		
+		return reviewList;
+	} //getReviewList() 끝 (리스트 가져오기)
+	
+	public reviewDTO getBoard(int RE_num) {
+		
+		reviewDTO dto = null;
+		PreparedStatement pstmt = null;
+		Connection con = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			con = getConnection();
+			String sql = "select * from board where num=?, id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, RE_num);
+			pstmt.setString(2, );
+			
+			rs=pstmt.executeQuery();
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		return;
 	}
+	
 	
 }
